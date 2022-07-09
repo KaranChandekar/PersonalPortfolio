0766daa233ddaad9ffb87337e8d5712f622ad4f2
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-09 17:46:51 +0530

    added menu fade animation

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const nav = document.querySelector(".nav");
 const navLinks = document.querySelector("#nav__links");
 
 // Page navigation
@@ -13,3 +14,21 @@ navLinks.addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing 'argument' into handler
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+
+nav.addEventListener("mouseout", handleHover.bind(1));
